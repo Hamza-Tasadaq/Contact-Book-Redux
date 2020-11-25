7ee3f5912d2820cfbaf56695c5b62fc0d50800dd
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2020-11-25 21:30:43 +0500

    form validation

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -29,15 +29,29 @@ class CreateContact extends Component {
     }
 
     createContact = () => {
+        let isEmpty;
+        let key = this.state;
         let history = this.props.history;
-        this.props.createContact(this.state, history);
-        this.setState({
-            firstName: "",
-            lastName: "",
-            phone: "",
-            email: "",
-            address: ""
-        })
+        for (var data in key) {
+            if (key[data] === '') {
+                isEmpty = true;
+                break;
+            }
+        }
+        if (isEmpty) {
+            alert("Please Fill The All The Inputs");
+            console.log(this.state)
+        }
+        else {
+            this.props.createContact(this.state, history);
+            this.setState({
+                firstName: "",
+                lastName: "",
+                phone: "",
+                email: "",
+                address: ""
+            })
+        }
     }
 
     render() {
@@ -48,14 +62,13 @@ class CreateContact extends Component {
                     <div className="h3 text-center form-heading">
                         Add Contact
                     </div>
-                    <form onSubmit={() => this.createContact}>
+                    <form>
                         <Input placeholder='First Name' name='firstName' value={this.state.firstName} handleChange={(e) => this.handleChange(e)} />
                         <Input placeholder='Last Name' name='lastName' value={this.state.lastName} handleChange={(e) => this.handleChange(e)} />
                         <Input placeholder='Phone' name='phone' value={this.state.phone} handleChange={(e) => this.handleChange(e)} />
                         <Input placeholder='Email' name='email' value={this.state.email} handleChange={(e) => this.handleChange(e)} />
                         <Input placeholder='Address' name='address' value={this.state.address} handleChange={(e) => this.handleChange(e)} />
                         <Link type="submit" onClick={() => this.createContact()} className="btn  btn-lg btn-block">Create Contact</Link>
-                        {/* to='/contacts' */}
                     </form>
                 </div>
             </div>
